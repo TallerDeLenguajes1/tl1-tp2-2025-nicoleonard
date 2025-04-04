f5b031b54b77e95675c187437fcbe71b9e1d3d11
Nico <nikolasleonard@gmail.com>
2025-04-04 09:28:55 -0300

    agregado archivo tp2_4, agregado punto 4a

---
+++ tp2_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct compu{
+    int velocidad;      // Velocidad de procesamiento en GHz (valor entre 1 y 3) 
+    int anio;           // Año de fabricación (valor entre 2015 y 2024) 
+    int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8) 
+    char *tipo_cpu;     // Tipo de procesador (apuntador a cadena de caracteres)    
+}compu;
+
+int main(){
+}
