92fb3bfb7e5c3449165e0103e641dd861d206db6
Nico <nikolasleonard@gmail.com>
2025-05-11 19:35:12 -0300

    corregidos errores en funciones. Punto 4 terminado

--- tp2_4.c
+++ tp2_4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct compu{
     int velocidad;      // Velocidad de procesamiento en GHz (valor entre 1 y 3) 
@@ -21,7 +22,9 @@ void mostrarMasVeloz(compu pcs[], int cantidad);
 int main(){
     compu listaCompus[5];
     generarCompus(listaCompus);
-
+    listarPCs(listaCompus,5);
+    mostrarMasVeloz(listaCompus, 5);
+    mostrarMasVieja(listaCompus, 5);
 }
 
 
@@ -30,38 +33,52 @@ void generarCompus(compu *listaCompu){
         (listaCompu+i)->velocidad= rand()%3+1;
         (listaCompu+i)->anio= 2015+rand()%10;
         (listaCompu+i)->cantidad_nucleos= rand()%8+1;
-        (listaCompu+i)->tipo_cpu[rand()%6];
+        (listaCompu+i)->tipo_cpu =tipos[rand()%6];
     }
 }
 
 void listarPCs(compu listaCompu[], int cantidadElementos){
+    printf("Lista PC \n");
     for(int i=0; i<cantidadElementos; i++){
-        printf("velocidad: %d - ", listaCompu[i].velocidad);
+        printf("velocidad: %d Ghz- ", listaCompu[i].velocidad);
         printf("cantidad de nucleos: %d - ", listaCompu[i].cantidad_nucleos);
         printf("tipo de cpu: %s - ", listaCompu[i].tipo_cpu);
         printf("año de fabricacion: %d", listaCompu[i].anio);
-        printf("\n --------------------------- ");
+        printf("\n --------------------------- \n");
     }   
 }
 
 void mostrarMasVieja(compu listaCompu[], int cantidadElementos){
     int indicePCVieja;
-    for(int i=0; i<cantidadElementos-1; i++){
-        if(listaCompu[i].anio <= listaCompu[i+1].anio){
+    int auxAnio =2025;
+    for(int i=0; i<cantidadElementos; i++){
+        if(listaCompu[i].anio <= auxAnio){
             indicePCVieja=i;
+            auxAnio=listaCompu[i].anio;
         }
     }
-    compu listaMostrar[0]=listaCompu[indicePCVieja];
-    listarPCs(listaMostrar,1);
+    printf("\n pc mas vieja \n");
+    printf("velocidad: %d Ghz- ", listaCompu[indicePCVieja].velocidad);
+    printf("cantidad de nucleos: %d - ", listaCompu[indicePCVieja].cantidad_nucleos);
+    printf("tipo de cpu: %s - ", listaCompu[indicePCVieja].tipo_cpu);
+    printf("año de fabricacion: %d", listaCompu[indicePCVieja].anio);
+    printf("\n --------------------------- \n");
+    
 }
 
 void mostrarMasVeloz(compu listaCompu[], int cantidadElementos){
-    int indicePCVieja;
-    for(int i=0; i<cantidadElementos-1; i++){
-        if(listaCompu[i].velocidad <= listaCompu[i+1].velocidad){
-            indicePCVieja=i;
+    int indicePCVeloz;
+    int auxVelocidad =0;
+    for(int i=0; i<cantidadElementos; i++){
+        if(listaCompu[i].velocidad >= auxVelocidad){
+            indicePCVeloz=i;
+            auxVelocidad=listaCompu[i].velocidad;
         }
     }
-    compu listaMostrar[0]=listaCompu[indicePCVieja];
-    listarPCs(listaMostrar,1);
+    printf("\n pc mas veloz \n");
+    printf("velocidad: %d Ghz- ", listaCompu[indicePCVeloz].velocidad);
+    printf("cantidad de nucleos: %d - ", listaCompu[indicePCVeloz].cantidad_nucleos);
+    printf("tipo de cpu: %s - ", listaCompu[indicePCVeloz].tipo_cpu);
+    printf("año de fabricacion: %d", listaCompu[indicePCVeloz].anio);
+    printf("\n --------------------------- \n");
 }
