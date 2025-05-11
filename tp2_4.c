78dc973dd7fc5083bd28a227a875526d1ca51c65
Nico <nikolasleonard@gmail.com>
2025-05-11 12:27:04 -0300

    agregada funcion para listar pcs

--- tp2_4.c
+++ tp2_4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct compu{
     int velocidad;      // Velocidad de procesamiento en GHz (valor entre 1 y 3) 
     int anio;           // Año de fabricación (valor entre 2015 y 2024) 
     int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8) 
@@ -12,6 +12,11 @@ char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
     "Pentium"};
 
 void generarCompus(compu *compu);
+void listarPCs(compu pcs[], int cantidad);
+void mostrarMasVieja(compu pcs[], int cantidad);
+void mostrarMasVeloz(compu pcs[], int cantidad);
+
+
 
 int main(){
     compu listaCompus[5];
@@ -21,12 +26,21 @@ int main(){
 
 
 void generarCompus(compu *listaCompu){
-    int i;
- 
-    for(i=0;i<5;i++){
+    for(int i=0;i<5;i++){
         (listaCompu+i)->velocidad= rand()%3+1;
         (listaCompu+i)->anio= 2015+rand()%10;
         (listaCompu+i)->cantidad_nucleos= rand()%8+1;
         (listaCompu+i)->tipo_cpu[rand()%6];
     }
 }
+
+void listarPCs(compu listaCompu[], int cantidadElementos){
+    for(int i=0; i<cantidadElementos; i++){
+        printf("velocidad: %d - ", listaCompu[i].velocidad);
+        printf("cantidad de nucleos: %d - ", listaCompu[i].cantidad_nucleos);
+        printf("tipo de cpu: %s - ", listaCompu[i].tipo_cpu);
+        printf("año de fabricacion: %d", listaCompu[i].anio);
+        printf("\n --------------------------- ");
+    }   
+}
+
